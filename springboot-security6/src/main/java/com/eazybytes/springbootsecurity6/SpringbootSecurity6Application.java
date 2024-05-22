b42e16a91f8044520ac4e617a200203ce545c933
tnischal <130941294+tnischal@users.noreply.github.com>
2024-05-22 16:43:00 -0500

    Add files via upload

---
+++ springboot-security6/src/main/java/com/eazybytes/springbootsecurity6/SpringbootSecurity6Application.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springbootsecurity6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurity6Application.class, args);
+	}
+
+}
---
+++ springboot-security6/src/test/java/com/eazybytes/springbootsecurity6/SpringbootSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springbootsecurity6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
