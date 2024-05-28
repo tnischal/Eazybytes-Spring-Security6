c0c15336719b238c81372b57467f435285ea213d
NischalTimsina <itstnischal@gmail.com>
2024-05-28 13:25:55 -0500

    add Cors Configuration

--- springboot-security6/src/main/java/com/eazybytes/springbootsecurity6/config/CostumLoginCreation/CustomerUserDetailService.java
+++ springboot-security6/src/main/java/com/eazybytes/springbootsecurity6/config/CostumLoginCreation/CustomerUserDetailService.java
@@ -1,42 +1,42 @@
-package com.eazybytes.springbootsecurity6.config.CostumLoginCreation;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class CustomerUserDetailService implements UserDetailsService {
-
-    @Autowired
-    private CustomerRepo customerRepo;
-
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String userName, password;
-        List<GrantedAuthority> authorities;
-        Optional<CustomerEntity> customerEntity= customerRepo.findByEmail(username);
-        boolean a = customerEntity.isPresent();
-        if(!customerEntity.isPresent()){
-            throw new UsernameNotFoundException("User details not found for the user:"+ username);
-        } else {
-            CustomerEntity customer = customerEntity.get();
-            userName = customer.getEmail();
-            password = customer.getPwd();
-            authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority(customer.getRole()));
-        }
-        return new User(userName,password,authorities);
-
-    }
-}
+//package com.eazybytes.springbootsecurity6.config.CostumLoginCreation;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class CustomerUserDetailService implements UserDetailsService {
+//
+//    @Autowired
+//    private CustomerRepo customerRepo;
+//
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        String userName, password;
+//        List<GrantedAuthority> authorities;
+//        Optional<CustomerEntity> customerEntity= customerRepo.findByEmail(username);
+//        boolean a = customerEntity.isPresent();
+//        if(!customerEntity.isPresent()){
+//            throw new UsernameNotFoundException("User details not found for the user:"+ username);
+//        } else {
+//            CustomerEntity customer = customerEntity.get();
+//            userName = customer.getEmail();
+//            password = customer.getPwd();
+//            authorities = new ArrayList<>();
+//            authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+//        }
+//        return new User(userName,password,authorities);
+//
+//    }
+//}
--- springboot-security6/src/main/java/com/eazybytes/springbootsecurity6/config/ProjectSecurityConfig.java
+++ springboot-security6/src/main/java/com/eazybytes/springbootsecurity6/config/ProjectSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.eazybytes.springbootsecurity6.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,8 +15,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -23,11 +31,25 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
 
-        http
-                .csrf((csrf)-> csrf.disable())
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/account", "/balance", "/loan", "/card").authenticated()
-                        .requestMatchers("/notice", "/contact","/signup","/register").permitAll())
+
+        http.securityContext((context) -> context
+                        .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .authorizeHttpRequests((requests)->requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
